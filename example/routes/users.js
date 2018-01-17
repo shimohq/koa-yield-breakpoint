274696b7b63befb2ee04d4cbcdf36b05a5e603ee
nswbmw <gxqzk@126.com>
2018-01-17 17:57:38 +0800

    update example

--- example/routes/users.js
+++ example/routes/users.js
@@ -1,8 +1,38 @@
-const User = require('../models/users')
+const Mongolass = require('mongolass')
+const mongolass = new Mongolass('mongodb://localhost:27017/test')
+const User = mongolass.model('User')
+const Post = mongolass.model('Post')
+const Comment = mongolass.model('Comment')
 
-exports.createUser = function * createUser () {
+exports.createUser = function * () {
   const name = this.query.name || 'default'
   const age = +this.query.age || 18
-  yield User.createUser(name, age)
+  yield createUser(name, age)
   this.status = 204
+}
+
+function * createUser (name, age) {
+  const user = (yield User.create({
+    name,
+    age
+  })).ops[0]
+  yield createPost(user)
+}
+
+function * createPost (user) {
+  const post = (yield Post.create({
+    uid: user._id,
+    title: 'post',
+    content: 'post'
+  })).ops[0]
+
+  yield createComment(user, post)
+}
+
+function * createComment (user, post) {
+  yield Comment.create({
+    userId: user._id,
+    postId: post._id,
+    content: 'comment'
+  })
 }
